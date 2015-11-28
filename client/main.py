4aff2ef7342d45949c877631774627da2f5c1868
Pedro Flemming <pedro.flemming@valsight.com>
2015-11-28 12:34:43 +0100

    added dummy python files

---
+++ client/main.py
@@ -0,0 +1,8 @@
+
+def main():
+	print "Running SCS server"
+	pass
+
+if __name__ == '__main__':
+	main()
+
---
+++ server/main.py
@@ -0,0 +1,8 @@
+
+def main():
+	print "Running SCS client"
+	pass
+
+if __name__ == '__main__':
+	main()
+
